16abd82586a3a7589ae709abe73f29ca8fe9a96d
Quint Stoffers <quint@swydo.com>
2019-10-24 11:57:34 +0200

    Add start-api command

---
+++ modules/byol-cli/src/commands/startApi.js/startApiServer.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { invokeApi } = require('@swydo/byol');
+const debug = require('debug')('byol:server');
+
+const app = express();
+
+app.all('*', (req, res) => {
+    let body = '';
+
+    req.on('data', (chunk) => {
+        body += chunk;
+    });
+
+    req.on('end', () => {
+        const httpMethod = req.method;
+        const path = req.url;
+
+        invokeApi(httpMethod, path, body)
+            .then((result) => {
+                res.send(result);
+            })
+            .catch((err) => {
+                res.status(500);
+                res.send(err);
+                res.end();
+            });
+    });
+});
+
+function startApiServer(port) {
+    app.listen(port);
+
+    debug('Listening on port', port);
+}
+
+module.exports = {
+    startApiServer,
+};
