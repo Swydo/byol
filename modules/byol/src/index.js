663c2769373029a50f82528b9311e197d9649b08
Quint Stoffers <quint@swydo.com>
2020-11-24 18:43:07 +0100

    Add getSqsMapping

--- modules/byol/src/index.js
+++ modules/byol/src/index.js
@@ -1,4 +1,5 @@
 const { getApiMapping } = require('./getApiMapping');
+const { getSqsMapping } = require('./getSqsMapping');
 const { generateRequestId } = require('./generateRequestId');
 const { invokeFunction } = require('./invokeFunction');
 const { invokeHandler } = require('./invokeHandler');
@@ -6,6 +7,7 @@ const { invokeHandler } = require('./invokeHandler');
 module.exports = {
     generateRequestId,
     getApiMapping,
+    getSqsMapping,
     invokeFunction,
     invokeHandler,
 };
