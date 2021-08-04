705ce809d9738bac1b39136353dc2fee61001721
Serge Bekenkamp <serge@swydo.com>
2021-08-04 15:47:36 +0200

    Rewrite webSocket server to use include correct events and to work with new api definition

--- modules/byol-cli/src/lib/startWebSocketServer.js
+++ modules/byol-cli/src/lib/startWebSocketServer.js
@@ -1,85 +1,193 @@
-
+const debug = require('debug')('byol:server:ws');
 const WebSocket = require('ws');
 const { invokeFunction } = require('@swydo/byol');
 const { generateRequestId } = require('@swydo/byol');
 const { getWebSocketMapping } = require('@swydo/byol');
+const dateformat = require('dateformat');
 
-
+const EVENTTYPE = {
+    CONNECT: 'CONNECT',
+    DISCONNECT: 'DISCONNECT',
+    MESSAGE: 'MESSAGE',
+}
 
 async function startWebSocketServer({
-    webSocket,
+    express,
     environmentOptions: {
         port = 3000,
     } = {},
     invokeOptions,
 }) {
-    const wss = new WebSocket.Server({
-        port: 11001,
-        clientTracking: true,
-    });
+    port = port + 1;
+    debug(`Listening on port: ${port}`);
+
+
     const connections = new Map();
+    const apiMap = getWebSocketMapping(invokeOptions.templatePath);
+    printConfiguration(apiMap);
 
 
-    const { routeSelectionExpression, routes } = getWebSocketMapping(invokeOptions.templatePath);
-    const onConnect = routes.has('$connect') ? invokeRouteFunction(routes.get('$connect'), invokeOptions) : () => {}
-    const onDisconnect = routes.has('$disconnect') ? invokeRouteFunction(routes.get('$disconnect'), invokeOptions) : () => {}
-
-
-    wss.on('connection', function (ws, request) {
-        const connectionId = generateRequestId();
-        connections.set(connectionId, ws);
-        onConnect(ws);
-
-
-        ws.on('message', onMessage(routes, routeSelectionExpression, ws, invokeOptions, connectionId));
-
-        ws.on('close', function () {
-            onDisconnect(ws);
-            connections.delete(id);
+    for (const [apiKey, api] of apiMap.entries()) {
+        // Todo: in the future if we want to support multiple stages
+        const wss = new WebSocket.Server({
+            port,
+            clientTracking: true,
+            path: '/$default'
         });
-    });
-}
 
-function invokeRouteFunction(route, invokeOptions) {
-    return (ws) => {
-        const functionName = route.lambdaName
 
-        invokeFunction(functionName, { }, invokeOptions)
-            .catch((err) => {
-                console.log(err);
-                ws.close(1006, JSON.stringify(err));
+        const routes = api.routes;
+        const defaultRoute = api.routes.get('$default');
+        const apiInfo = {
+            apiId: apiKey,
+        };
+
+        wss.on('connection', function (ws, request) {
+            const connectionContext = {
+                connectionId: generateRequestId(),
+                connectedAt: new Date().getTime(),
+                ip: request.socket.remoteAddress,
+            };
+            connections.set(connectionContext.connectionId, ws);
+            onConnect(routes.get('$connect'), apiInfo, invokeOptions, ws, request, connectionContext);
+
+
+            ws.on('message', (message) => {
+                const event = {
+                    requestContext: getRequestContext(defaultRoute, connectionContext, apiInfo, EVENTTYPE.MESSAGE),
+                    body: message,
+                    isBase64Encoded: false,
+                };
+
+                const functionName = defaultRoute.lambdaName;
+                debug(`Invoke route '$default' with lambda ${functionName} at ws`);
+                invokeFunction(defaultRoute.lambdaName, event, invokeOptions)
+                    .then(result => {
+                        ws.send(JSON.stringify(result.result.body));
+                    })
+                    .catch((err) => {
+                        debug('error when handling message', err);
+                        ws.close(1006, JSON.stringify(err));
+                    });
             });
+
+            ws.on('close', function () {
+                try {
+                    onDisconnect(routes.get('$disconnect'), apiInfo, invokeOptions, ws, request, connectionContext);
+                    connections.delete(connectionContext.connectionId);
+                } catch (e) {
+                    debug(e);
+                }
+            });
+        });
     }
 }
 
-function onMessage(routes, routeSelectionExpression, ws, invokeOptions, connectionId) {
-    const routeSelector = buildRouteSelector(routeSelectionExpression);
-    const defaultRoute = routes.get('$default');
-    return (message) => {
-        const selectedRoute = routeSelector(message);
-        let route = defaultRoute;
-        if(routes.has(selectedRoute)) {
-            route = routes.get(selectedRoute);
-        }
+function getLambdaEvent(headers, routeConfig, connectionContext, apiInfo, eventType) {
+    const event = {
+        "headers": headers,
+        "multiValueHeaders": headersToMultiValueHeaders(headers),
+        "requestContext": getRequestContext(routeConfig, connectionContext, apiInfo, eventType),
+        "isBase64Encoded": false,
+    };
+    return event;
+}
 
-        invokeFunction(route.lambdaName, { requestContext: {connectionId} }, invokeOptions)
+function onConnect(routeConfig, apiInfo, invokeOptions, ws, request, connectionContext) {
+    if (routeConfig) {
+        const headers =  {
+            "Host": request.headers.host,
+            "Sec-WebSocket-Extensions": request.headers['sec-websocket-extensions'],
+            "Sec-WebSocket-Key": request.headers['sec-websocket-key'],
+            "Sec-WebSocket-Version": request.headers['sec-websocket-version'],
+            "X-Amzn-Trace-Id": "Root=1-6107c109-306f421006007da9658753b3",
+            "X-Forwarded-For": request.socket.remoteAddress,
+            "X-Forwarded-Port": "443",
+            "X-Forwarded-Proto": "https",
+        };
+        const event = getLambdaEvent(headers, routeConfig, connectionContext, apiInfo, EVENTTYPE.CONNECT);
+        debug(routeConfig.lambdaName, 'at ws', routeConfig.route.Properties.routeKey);
+        invokeFunction(routeConfig.lambdaName, event, invokeOptions)
             .then(result => {
-                ws.send(JSON.stringify(result.result.body));
+                const responseCode = result.result.responseCode ?? 500;
+
+                if (responseCode < 200  || 400 <= responseCode) {
+                    ws.terminate();
+                }
             })
             .catch((err) => {
-                console.log(err);
-                ws.close(1006, JSON.stringify(err));
+                terminateConnection(ws, connectionContext)
             });
+    } else {
+        terminateConnection(ws, connectionContext)
     }
 }
 
-function buildRouteSelector(routeSelectionExpression) {
-    return (message) => {
-        return '$default';
+function terminateConnection(ws, connectionContext) {
+    debug(`Terminated connection ${connectionContext.connectionId}`);
+    ws.terminate();
+}
+
+function onDisconnect(routeConfig, apiInfo, invokeOptions, ws, request, connectionContext) {
+    if (routeConfig) {
+        const headers =  {
+            "Host": request.headers.host,
+            "x-api-key": "",
+            "X-Forwarded-For": "",
+            "x-restapi": "",
+        };
+        const event = getLambdaEvent(headers, routeConfig, connectionContext, apiInfo, EVENTTYPE.DISCONNECT);
+        debug(routeConfig.lambdaName, 'at ws', routeConfig.route.Properties.routeKey);
+        invokeFunction(routeConfig.lambdaName, event, invokeOptions)
+            .catch((err) => {
+                terminateConnection(ws, connectionContext);
+            });
+    } else {
+        ws.close();
     }
 }
 
+function getRequestContext(routeConfig, connectionContext, apiInfo, eventType) {
+    const requestId = generateRequestId();
+    return {
+        "routeKey": routeConfig.route.routeKey,
+        "disconnectStatusCode": -1,
+        "eventType": eventType,
+        "extendedRequestId": requestId,
+        "requestTime": getFormattedDate(new Date()),
+        "messageDirection": "IN",
+        "disconnectReason": "",
+        "stage": "$default",
+        "connectedAt": connectionContext.connectedAt,
+        "requestTimeEpoch": new Date().getTime(),
+        "identity": {
+            "sourceIp": connectionContext.ip,
+        },
+        "requestId": requestId,
+        "domainName": "localhost",
+        "connectionId": connectionContext.connectionId,
+        "apiId": apiInfo.apiId,
+    };
+}
+
+function headersToMultiValueHeaders(headers) {
+    return Object.keys(headers).reduce(((previousValue, currentValue) => ({[currentValue]: [headers[currentValue]], ...previousValue})), {});
+}
+
+function printConfiguration(apiMap) {
+    for (const [apiKey, api] of apiMap.entries()) {
+        debug(`Registering api: ${apiKey}`);
+        for (const [key, route] of api.routes.entries()) {
+            debug(`Route: ${key} with lambda ${route.lambdaName}`);
+        }
+    }
+}
+
+function getFormattedDate(date) {
+    //format as such 02/Aug/2021:09:55:21 +0000
+    dateformat(date, 'UTC:dd/mmm/yyyy:hh:MM:ss +0000');
+}
 
 module.exports = {
     startWebSocketServer,
-}
+};
