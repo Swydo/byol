20ff14d4d77f10c772ce591d6ab2a68d7d2f9913
Serge Bekenkamp <serge@swydo.com>
2021-08-04 15:46:56 +0200

    Update mapping to include all config per route and api.

--- modules/byol/src/getWebSocketMapping.js
+++ modules/byol/src/getWebSocketMapping.js
@@ -1,30 +1,35 @@
-const { getResources } = require('./resources/getResources');
 const { getFunctionResources } = require('./resources/getFunctionResources');
 const { getTemplate } = require('./getTemplate');
 
-function toMap(obj) {
+function toFilteredMap(obj, filter) {
     const map = new Map();
-    Object.keys(obj).forEach(key => map.set(key, obj[key]));
+    Object.keys(obj).forEach(key => {
+        if (filter(obj[key])) {
+            map.set(key, obj[key]);
+        }
+    });
     return map;
 }
 
-function resolveReferences(templatePath, target, type) {
-    const template = getTemplate(templatePath);
-    const map = new Map();
-    if(typeof target === 'string'){
-        map.set(target, templat[target]);
-    }
-    else if (typeof target === 'object') {
+function getYamlVariables(target) {
+    if (typeof target === 'string') {
+        return [{
+            value: target,
+            start: 0,
+            end: target.length
+        }];
+    } else if (typeof target === 'object') {
         const key = Object.keys(target)[0];
 
         switch (key) {
             case 'Fn::Sub': {
                 const regx = new RegExp(/\$\{([\D]+?)\}/g);
                 const references = Array.from(target[key].matchAll(regx));
-                references.forEach(item => {
-                    const split = item[1].split('.')
-                    if(!type || isResourceType(template.Resources[split[0]], type)) {
-                        map.set(split[0], template.Resources[split[0]])
+                return references.map(item => {
+                    return {
+                        value: item[1],
+                        start: item.index,
+                        end: item[0].length,
                     }
                 });
                 break;
@@ -33,47 +38,108 @@ function resolveReferences(templatePath, target, type) {
                 break;
         }
     }
-
-    return map;
 }
 
-function isResourceType(resource, type) {
-    return resource?.Type === type;
+
+const WEBSOCKET_PROTOCOL_TYPE = 'WEBSOCKET';
+const API_GATEWAY_RESOURCE = 'AWS::ApiGatewayV2::Api'
+
+function getWebSocketApis(template) {
+    const apis = getResources(template, API_GATEWAY_RESOURCE);
+    const webSocketApis = toFilteredMap(apis, api => api.Properties.ProtocolType === WEBSOCKET_PROTOCOL_TYPE);
+    return webSocketApis;
 }
 
-function getIntegrationLambdaName(templatePath, integration) {
-    const references = resolveReferences(templatePath, integration.Properties.IntegrationUri, 'AWS::Serverless::Function')
-    for(const [key, value] of references) {
-        if(value.Type === 'AWS::Serverless::Function') {
-            return key;
+function getResources(template, resourceType) {
+    return Object
+        .keys(template.Resources)
+        .filter((resourceKey) => template.Resources[resourceKey].Type === resourceType)
+        .reduce((all, resourceKey) => ({
+            ...all,
+            [resourceKey]: template.Resources[resourceKey],
+        }), {});
+}
+
+function getRoutesForApi(template, apiName) {
+
+    return toFilteredMap(getResources(template, 'AWS::ApiGatewayV2::Route'), (route) => route.Properties.ApiId.Ref === apiName);
+}
+
+function getIntegrationsForApi(template, apiName) {
+    return toFilteredMap(getResources(template, 'AWS::ApiGatewayV2::Integration'), (route) => route.Properties.ApiId.Ref === apiName);
+}
+
+function getRouteResponsesForApi(template, apiName) {
+    return toFilteredMap(getResources(template, 'AWS::ApiGatewayV2::RouteResponse'), (route) => route.Properties.ApiId.Ref === apiName);
+}
+
+function getIntegrationResponsesForApi(template, apiName) {
+    return toFilteredMap(getResources(template, 'AWS::ApiGatewayV2::IntegrationResponse'), (route) => route.Properties.ApiId.Ref === apiName);
+}
+
+
+function getIntegrationForRoute(route, integrations) {
+    let variables = getYamlVariables(route.Properties.Target);
+    return variables.map(item => integrations.has(item.value) ? integrations.get(item.value) : undefined).filter(item => !!item)[0];
+}
+
+function getIntegrationResponseForRoute(integration, integrationsResponses, integrations) {
+    return Array.from(integrationsResponses.entries()).map(([integrationResponseName, integrationResponse]) => {
+        if (integrationResponse.Properties.IntegrationId) {
+            if(integration === integrations.get(integrationResponse.Properties.IntegrationId.Ref)) {
+                return integrationResponse;
+            }
+
+        }
+    }).find(x=> !!x);
+
+}
+
+function getLambdaNameFromIntegration(integration, lambdaFunctions) {
+       const yamlVariables = getYamlVariables(integration.Properties.IntegrationUri);
+       return yamlVariables
+           .flatMap(item => item.value.split('.'))
+           .filter(name => lambdaFunctions.has(name))[0];
+}
+
+function getRouteResponse(routeName, apiRouteResponses) {
+    for(let [key, value] in apiRouteResponses) {
+        if(value.Properties.RouteId.Ref === routeName) {
+            return value;
         }
     }
-
-
-    return undefined;
 }
 
+function getApiGatewayRoutes(apiName, template) {
+    const apiRoutes = getRoutesForApi(template, apiName);
+    const apiRouteResponses = getRouteResponsesForApi(template, apiName);
+    const apiIntegrations = getIntegrationsForApi(template, apiName);
+    const apiIntegrationResponses = getIntegrationResponsesForApi(template, apiName);
+    const lambdaFunctions = new Map(Object.entries(getResources(template, 'AWS::Serverless::Function')));
 
+    const routes = new Map();
 
-function getApiGatewayRoutes(templatePath) {
-    const mappedRoutes = new Map();
-    const routes = toMap(getResources(templatePath, 'AWS::ApiGatewayV2::Route'));
+    for(const [routeName, route] of apiRoutes.entries()) {
+        const routeResponse = getRouteResponse(routeName, apiRouteResponses)
 
-    routes.forEach((value, key) => {
-        const references = resolveReferences(templatePath, value.Properties.Target, 'AWS::ApiGatewayV2::Integration');
-        const [_, integration] = [...references.entries()][0];
-        if (integration) {
-           const lambda = getIntegrationLambdaName(templatePath, integration);
-            mappedRoutes.set(value.Properties.RouteKey, {
-                route: value,
-                integration: integration,
-                lambdaName: lambda,
-            })
+        const integration = getIntegrationForRoute(route, apiIntegrations);
+        const integrationResponse = getIntegrationResponseForRoute(integration, apiIntegrationResponses, apiIntegrations)
 
+        const lambdaName = getLambdaNameFromIntegration(integration, lambdaFunctions)
+
+        const x = {
+            route,
+            routeResponse,
+            integration,
+            integrationResponse,
+            lambdaName,
+            lambda: lambdaFunctions.get(lambdaName),
         }
-    });
+        routes.set(route.Properties.RouteKey, x);
+    }
 
-    return mappedRoutes;
+    return routes;
+
 }
 
 function getRouteSelector(templatePath) {
@@ -83,12 +149,18 @@ function getRouteSelector(templatePath) {
 
 
 function getWebSocketMapping(templatePath) {
-    const mapping = getApiGatewayRoutes(templatePath);
-    const routeSelectionExpression = getRouteSelector(templatePath);
-    return {
-        routeSelectionExpression,
-        routes: mapping,
-    };
+    const template = getTemplate(templatePath);
+
+    const gatewayApis = getWebSocketApis(template);
+    const apis = new Map();
+    Array.from(gatewayApis.entries()).forEach(([key, api]) => {
+        apis.set(key, {
+            api,
+            routes: getApiGatewayRoutes(key, template),
+        });
+    })
+
+    return apis;
 }
 
 module.exports = {
