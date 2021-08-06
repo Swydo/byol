44d0eeb89e67688c620f84df6485a937d9e0f402
Serge Bekenkamp <serge@swydo.com>
2021-08-06 11:55:13 +0200

    add start websocket command

---
+++ modules/byol-cli/src/commands/startWebsocket/index.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const { startServer } = require('../../lib/startServer');
+const { handleGlobalOptions } = require('../../handleGlobalOptions');
+
+const command = ['start-api'];
+const desc = 'Start a local api server';
+const builder = (yargs) => yargs
+    .option('env-path', {
+        alias: ['env-vars', 'n'],
+        default: './env.json',
+    })
+    .option('keep-alive')
+    .option('port', {
+        alias: 'p',
+        default: 3000,
+    })
+    .option('profile', {
+        default: 'default',
+    })
+    .option('region')
+    .option('template-path', {
+        alias: ['template-file', 'template', 't'],
+        default: './template.yml',
+    });
+const handler = async ({
+    envPath,
+    keepAlive,
+    port,
+    profile,
+    region,
+    templatePath,
+    ...globalOptions
+}) => {
+    handleGlobalOptions(globalOptions);
+
+    startServer({
+        environmentOptions: {
+            port,
+        },
+        invokeOptions: {
+            keepAlive,
+            profile,
+            region,
+            envPath: path.resolve(process.cwd(), envPath),
+            templatePath: path.resolve(process.cwd(), templatePath),
+        },
+        webSocket: true,
+    });
+};
+
+module.exports = {
+    command,
+    desc,
+    builder,
+    handler,
+};
